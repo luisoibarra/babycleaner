e49ce835cfb58e8a628d7d69bb08ed1f5e669b7e
Luiso <luise98cu@gamil.com>
2022-02-04 10:49:01 -0500

    Layer Beh

--- src/LayerBehaviorRobot.hs
+++ src/LayerBehaviorRobot.hs
@@ -1,14 +1,34 @@
+module LayerBehaviorRobot where
+
 import BehaviorRobot
+import Data.List
+import BehaviorUtils
+import Environment
+import Agent
+import Debug.Trace
 
 
-layerBehaviorRobot = [
+robotLayerBehavior = [
         takeBabyToPlaypen,
         searchBaby,
         cleanDirt
+        -- getDoNothingAction
     ]
 
+layerSelectFunctionRobot :: [(Env s, Agent, b)] -> (Env s, b)
 layerSelectFunctionRobot possibleActions =
     let
-        robotPriority = sortOn
+        -- possibleActions = trace (show $ map snd _possibleActions) _possibleActions
+        (env, agent, actions) = head $ sortOn getRobotBehPriority possibleActions
     in
-        (0, [])
+        -- trace (show (env, actions)) (env, actions)
+        (env, actions) 
+
+getRobotBehPriority (env, agent, actions)
+    | trace ("Holding" ++ show (isCurrentlyHoldingBaby env agent)) isCurrentlyHoldingBaby env agent = 0
+    | trace ("Free Babies" ++ show (existFreeBabies env agent)) existFreeBabies env agent = 1
+    | trace ("Env Dirty" ++ show (isEnvDirty env agent)) isEnvDirty env agent = 2
+    | isCurrentlyHoldingBaby env agent = 0
+    | existFreeBabies env agent = 1
+    | isEnvDirty env agent = 2
+    | otherwise = 3
